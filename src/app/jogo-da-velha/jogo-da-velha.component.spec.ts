6f668807fc4db87d9f255d911b0ff3c9ccd616d8
koetz05 <felipekoetz@gmail.com>
2021-01-08 17:19:48 -0300

    Initial commit

---
+++ src/app/jogo-da-velha/jogo-da-velha.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {JogoDaVelhaService} from './shared';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+
+describe('JogoDaVelhaComponent', () => {
+  let component: JogoDaVelhaComponent;
+  let fixture: ComponentFixture<JogoDaVelhaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JogoDaVelhaComponent ],
+      providers: [JogoDaVelhaComponent]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JogoDaVelhaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
